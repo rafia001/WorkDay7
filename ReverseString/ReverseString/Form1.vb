c51f0590e5aabcbfcb698d72efd021a3f4b372d5
idsc3101 <idsc3101@gmail.com>
2013-10-16 16:07:35 -0500

    done

--- ReverseString/ReverseString/Form1.vb
+++ ReverseString/ReverseString/Form1.vb
@@ -5,15 +5,28 @@ Public Class Form1
     Private Sub btnStart_Click(sender As System.Object, e As System.EventArgs) Handles btnStart.Click
         ' Read the input from the user
         Dim st As String
+
         st = InputBox("Please type the initial string")
+
 
         ' Compute the result
         Dim result As String = ""
 
+        Dim a As Integer
+        Dim length As Integer
 
+        length = st.Length()
+
+        For a = length - 1 To 0 Step -1
+            Dim c As String
+            c = st.Chars(a)
+
+            result = result & c
+        Next
 
         ' Report the output to the user
-        MessageBox.Show("Not implemented yet", "Result")
+        MessageBox.Show(result, "Result")
+
 
     End Sub
 End Class
